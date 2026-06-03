ea5cbae114fbbdf65c68f4034e9717cff5833237
arham4486 <syedarham4486@gmail.com>
2026-06-03 19:05:22 -0400

    added feature engineering - success label, review velocity, star weight

--- analysis.py
+++ analysis.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-df = pd.read_json('yelp_academic_dataset_business.json/yelp_academic_dataset_business.json', lines=True)
+df = pd.read_json('yelp_academic_dataset_business.json', lines=True)
 print(df.head())
 print(df.columns)
 print(df.shape)
@@ -32,3 +32,10 @@ def recommend_restaurants(min_stars):
     filtered = df_philly[df_philly['stars'] >= min_stars]
     return filtered.sort_values('review_count', ascending = False).head(5)
 print(recommend_restaurants(4.0))
+df_philly['success'] = (df_philly['stars']>= 4.0) & (df_philly['review_count'] >= 50)
+success_counts = (df_philly['success'].value_counts())
+print(success_counts)
+df_philly['review_velocity'] = (df_philly['review_count']) / (df_philly['review_count'].max())
+print(df_philly['review_velocity'].head())
+df_philly['star_weight'] = (df_philly['stars']) * (df_philly['review_velocity'])
+print(df_philly['star_weight'].head())
